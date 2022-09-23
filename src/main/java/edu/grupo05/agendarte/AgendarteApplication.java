7eb763d7ed48b3738b05456999b9133b6f13bffb
Yonathan Galvez <yonathan.3801911625@ucaldas.edu.co>
2022-09-23 12:47:51 -0500

    crud user

---
+++ src/main/java/edu/grupo05/agendarte/AgendarteApplication.java
@@ -0,0 +1,13 @@
+package edu.grupo05.agendarte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendarteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendarteApplication.class, args);
+	}
+
+}
---
+++ src/main/java/edu/grupo05/agendarte/entity/User.java
@@ -0,0 +1,31 @@
+package edu.grupo05.agendarte.entity;
+
+// Importing required classes
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+// Annotations
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long userId;
+    private String userCedula;
+    private String userName;
+    private String userLastName;
+    private String userAddress;
+    private String userTelephone;
+    private String userMail;
+}
